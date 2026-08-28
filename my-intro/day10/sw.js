e2a9b2a78e3afe9e5e3b6fdc548d195ae42c1965
jys <emj2994@gmail.com>
2026-08-28 11:47:44 +0900

    day10: 물 한잔 PWA 폴더 추가 (히스토리 없이 병합)

---
+++ my-intro/day10/sw.js
@@ -0,0 +1,49 @@
+const CACHE_VERSION = 'v1';
+const CACHE_NAME = `water-tracker-${CACHE_VERSION}`;
+
+const PRECACHE_URLS = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(PRECACHE_URLS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(event.request)
+        .then((response) => {
+          if (response.ok) {
+            const clone = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          }
+          return response;
+        })
+        .catch(() => {
+          if (event.request.mode === 'navigate') return caches.match('./index.html');
+        });
+    })
+  );
+});
